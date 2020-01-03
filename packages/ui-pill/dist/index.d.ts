9144417cde259d8d4cbe36f1184e434b72fd78f4
Dzintars Klavins <dzintars.klavins@gmail.com>
2020-01-03 23:46:35 +0200

    chore: this and that

--- packages/ui-pill/dist/index.d.ts
+++ packages/ui-pill/dist/index.d.ts
@@ -1,1 +1,2 @@
 export * from './ui-pill';
+//# sourceMappingURL=index.d.ts.map
--- packages/ui-pill/dist/template.d.ts
+++ packages/ui-pill/dist/template.d.ts
@@ -1,2 +1,3 @@
 import { UiPill } from './ui-pill';
 export default function template(this: UiPill): import("lit-element").TemplateResult;
+//# sourceMappingURL=template.d.ts.map
